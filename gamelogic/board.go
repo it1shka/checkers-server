366975255fc99f9fb02cf09b28fd4176e92385f1
it1shka <2tishbel@gmail.com>
2024-11-04 15:31:59 +0100

    feat(board): added Board.PieceAt function

--- gamelogic/board.go
+++ gamelogic/board.go
@@ -25,6 +25,12 @@ type Board struct {
 	pieces []Piece
 }
 
+type BoardMove struct {
+  From PieceSquare
+  To PieceSquare
+  Hit bool
+}
+
 func InitBoard() Board {
 	pieces := make([]Piece, boardSize)
 	index := 0
@@ -94,3 +100,71 @@ func (board Board) String() string {
 	}
 	return output.String()
 }
+
+func (board Board) PieceAt(square PieceSquare) (Piece, bool) {
+	for _, piece := range board.pieces {
+		if piece.Square == square {
+			return piece, true
+		}
+	}
+	return Piece{}, false
+}
+
+func (board Board) PieceAllMovesAt(square PieceSquare) ([]BoardMove, bool) {
+	piece, exists := board.PieceAt(square)
+	if !exists || piece.Color != board.turn {
+		return nil, false
+	}
+	var directions []PiecePosition
+	switch {
+	case piece.Type == MAN && piece.Color == BLACK:
+		directions = []PiecePosition{{-1, -1}, {-1, 1}}
+	case piece.Type == MAN && piece.Color == RED:
+		directions = []PiecePosition{{1, -1}, {1, 1}}
+	default:
+		directions = []PiecePosition{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
+	}
+  current := piece.Square.ToPosition()
+  var moves []BoardMove
+  for _, direction := range directions {
+    next := PiecePosition{
+      Row: current.Row + direction.Row,
+      Column: current.Column + direction.Column,
+    }
+    if !next.IsValid() {
+      continue
+    }
+    nextSquare := next.ToSquare()
+    nextPiece, exists := board.PieceAt(nextSquare)
+    if !exists {
+      moves = append(moves, BoardMove{
+        From: piece.Square,
+        To: nextSquare,
+        Hit: false,
+      })
+      continue
+    }
+    if nextPiece.Color == piece.Color {
+      continue
+    }
+    doubleNext := PiecePosition{
+      Row: current.Row + direction.Row * 2,
+      Column: current.Column + direction.Column * 2,
+    }
+    if !doubleNext.IsValid() {
+      continue
+    }
+    doubleNextSquare := doubleNext.ToSquare()
+    if _, exists := board.PieceAt(doubleNextSquare); exists {
+      continue
+    }
+    moves = append(moves, BoardMove{
+      From: piece.Square,
+      To: doubleNextSquare,
+      Hit: true,
+    })
+  }
+  return moves, true
+}
+
+
--- gamelogic/board_test.go
+++ gamelogic/board_test.go
@@ -24,6 +24,38 @@ func TestBoardInit(t *testing.T) {
 	}
 }
 
-func Test(t *testing.T) {
-
+func TestBoardPieceAt(t *testing.T) {
+	board := gamelogic.InitBoard()
+	testCases := []struct {
+		Square gamelogic.PieceSquare
+		Color  gamelogic.PieceColor
+		Exists bool
+	}{
+		{Square: 1, Color: gamelogic.RED, Exists: true},
+		{Square: 2, Color: gamelogic.RED, Exists: true},
+    {Square: 13, Exists: false},
+    {Square: 14, Exists: false},
+    {Square: 21, Color: gamelogic.BLACK, Exists: true},
+    {Square: 25, Color: gamelogic.BLACK, Exists: true},
+	}
+  for _, testCase := range testCases {
+    piece, exists := board.PieceAt(testCase.Square)
+    switch {
+    case !testCase.Exists && exists:
+      t.Fatalf(
+        "expected %d square to be empty", 
+        testCase.Square,
+      )
+    case testCase.Exists && !exists:
+      t.Fatalf(
+        "expected %d square to contain a piece",
+        testCase.Square,
+      )
+    case testCase.Exists && exists && testCase.Color != piece.Color:
+      t.Fatalf(
+        "expected different color at %d square",
+        testCase.Square,
+      )
+    }
+  }
 }
