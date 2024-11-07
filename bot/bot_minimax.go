93de9c05668833b1c7d0c5f69199fec823976e7f
it1shka <2tishbel@gmail.com>
2024-11-07 12:07:11 +0100

    feat(bot): added the first implementation of the minimax

--- bot/bot_minimax.go
+++ bot/bot_minimax.go
@@ -2,9 +2,14 @@ package bot
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
+	"time"
 
 	"it1shka.com/checkers-server/gamelogic"
 )
+
+const winWeight float32 = 100.0
 
 func getCenterSquares() []gamelogic.PieceSquare {
 	return []gamelogic.PieceSquare{
@@ -74,11 +79,58 @@ func (bot BotMinimax) Name() string {
 }
 
 func (bot BotMinimax) Move(board gamelogic.Board) (gamelogic.BoardMove, bool) {
-	// TODO: complete this function
-	panic("Not implemented!")
+	moves := board.CurrentPossibleMoves()
+	if len(moves) <= 0 {
+		return gamelogic.BoardMove{}, false
+	}
+	var bestMove gamelogic.BoardMove
+	bestScore := float32(0.0)
+	rnd := rand.New(rand.NewSource(time.Now().Unix()))
+	rnd.Shuffle(len(moves), func(i, j int) {
+		moves[i], moves[j] = moves[j], moves[i]
+	})
+	for _, move := range moves {
+		nextBoard, _ := board.MakeMove(move.From, move.To)
+		score := bot.minimax(nextBoard, bot.depth, board.Turn())
+		if score > bestScore {
+			bestScore = score
+			bestMove = move
+		}
+	}
+	return bestMove, true
+}
+
+func (bot BotMinimax) minimax(board gamelogic.Board, depth uint, maximizing gamelogic.PieceColor) float32 {
+	if depth == 0 {
+		return bot.evaluate(board, maximizing)
+	}
+	moves := board.CurrentPossibleMoves()
+	if len(moves) <= 0 {
+		return bot.evaluate(board, maximizing)
+	}
+	output := float32(math.Inf(-1))
+	if board.Turn() != maximizing {
+		output = float32(math.Inf(1))
+	}
+	for _, move := range moves {
+		nextBoard, _ := board.MakeMove(move.From, move.To)
+		result := bot.minimax(nextBoard, depth-1, maximizing)
+		if board.Turn() == maximizing {
+			output = max(output, result)
+		} else {
+			output = min(output, result)
+		}
+	}
+	return output
 }
 
 func (bot BotMinimax) evaluate(board gamelogic.Board, player gamelogic.PieceColor) float32 {
+	if len(board.CurrentPossibleMoves()) <= 0 {
+		if board.Turn() == player {
+			return -winWeight
+		}
+		return winWeight
+	}
 	weight := float32(0.0)
 	for _, piece := range board.Pieces() {
 		coefficient := float32(1.0)
