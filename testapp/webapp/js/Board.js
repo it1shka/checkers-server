6a4787a95d8756e9586626707848b23a0598b6c6
it1shka <2tishbel@gmail.com>
2024-11-17 14:13:25 +0100

    progress(testapp/webapp): doing the frontend for my testapp

--- testapp/webapp/js/Board.js
+++ testapp/webapp/js/Board.js
@@ -1,5 +1,17 @@
 import { computed } from 'vue'
 import useSettingsState from './useSettingsState.js'
+import useWebsocket from './useWebsocket.js'
+
+const isSquare = index => {
+  const row = ~~(index / 8)
+  return row % 2 == 0
+    ? index % 2 !== 0
+    : index % 2 === 0
+}
+
+const squareIndex = index => {
+  return ~~(index / 2) + 1
+}
 
 export default {
   setup() {
@@ -7,24 +19,46 @@ export default {
     const botName = computed(() => settingsState.bot)
     const yourColor = computed(() => settingsState.color)
 
+    const websocket = useWebsocket()
+    const { closeConnection } = websocket
+
     return {
       botName,
       yourColor,
+      isSquare,
+      squareIndex,
+      closeConnection,
     }
   },
   template: `
+    <button @click="closeConnection" class="btn-disconnect">
+      Disconnect
+    </button>
+    <button class="btn-restart">
+      Restart
+    </button>
     <div class="board">
       <div class="board__statusbar">
         <p>Bot: {{ botName }}</p>
-        <p>Your color: {{ yourColor }}</p>
+        <p>
+          Your color: 
+          <span :style="{ color: yourColor }">
+            {{ yourColor }}
+          </span>
+        </p>
       </div>
       <div class="board__grid">
         <div
-          class="board__grid__cell"
-          v-for="index in 64"
+          v-for="(_, index) in 64"
           :key="index"
+          :class="{
+            board__grid__cell: true,
+            board__grid__square: isSquare(index),
+          }"
         >
-          {{ index }}
+          <p v-if="isSquare(index)">
+            {{ squareIndex(index) }}
+          </p>
         </div>
       </div>
       <div class="board__turn">
