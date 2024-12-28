03b650d0a8fabcbd69033f31adcfd4b0aa3e48dd
it1shka <2tishbel@gmail.com>
2024-12-28 15:53:00 +0100

    cleaned everything for the 3rd time

--- multiplayer/server.go
+++ multiplayer/server.go
@@ -8,42 +8,14 @@ import (
 
 	"github.com/gorilla/schema"
 	"github.com/gorilla/websocket"
-	"it1shka.com/checkers-server/gamelogic"
 )
-
-// Additional record types
-
-type RequestMetadata struct {
-	Nickname string `schema:"nickname"`
-	Rating   uint   `schema:"rating"`
-	Region   string `schema:"region"`
-}
-
-type IncomingMessage struct {
-	Type    string          `json:"type"`
-	Payload json.RawMessage `json:"payload"`
-}
-
-const (
-	MSG_IN_JOIN  = "join"
-	MSG_IN_LEAVE = "leave"
-	MSG_IN_MOVE  = "move"
-)
-
-type OutcomingMessage struct {
-	Type    string `json:"type"`
-	Payload any    `json:"payload"`
-}
-
-// Server implementation
 
 const SERVER_READ_BUFFER_SIZE = 1024
 const SERVER_WRITE_BUFFER_SIZE = 1024
 
 type Server struct {
-	upgrader    websocket.Upgrader
-	decoder     *schema.Decoder
-	matchmaking *Matchmaking[Player]
+	upgrader websocket.Upgrader
+	decoder  *schema.Decoder
 }
 
 func NewServer() *Server {
@@ -52,10 +24,8 @@ func NewServer() *Server {
 			ReadBufferSize:  SERVER_READ_BUFFER_SIZE,
 			WriteBufferSize: SERVER_WRITE_BUFFER_SIZE,
 		},
-		decoder:     schema.NewDecoder(),
-		matchmaking: nil,
+		decoder: schema.NewDecoder(),
 	}
-	server.matchmaking = NewMatchmaking(server.handleMatch)
 	return server
 }
 
@@ -68,26 +38,13 @@ func (s *Server) Start(port string) {
 	}
 }
 
-func (s *Server) handleMatch(playerA, playerB Player) {
-	go func() {
-		playerA.NotifyAboutEnemy(playerB.Info())
-		playerB.NotifyAboutEnemy(playerA.Info())
-		session := NewSession(playerA.ID(), playerB.ID())
-		updateHandler := func(update Update) {
-			playerA.NotifyAboutUpdate(update)
-			playerB.NotifyAboutUpdate(update)
-			if update.Status != gamelogic.ACTIVE {
-				session.Stop()
-				playerA.Stop()
-				playerB.Stop()
-			}
-		}
-
-	}()
-}
-
 func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) {
-	var metadata RequestMetadata
+	var metadata struct {
+		Nickname string `schema:"nickname"`
+		Rating   uint   `schema:"rating"`
+		Region   string `schema:"region"`
+	}
+
 	if err := s.decoder.Decode(&metadata, r.URL.Query()); err != nil {
 		log.Println(err)
 		http.Error(w, "failed to parse your request", http.StatusBadRequest)
@@ -100,28 +57,20 @@ func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	go s.handleConnection(conn, metadata)
-}
-
-func (s *Server) handleConnection(conn *websocket.Conn, meta RequestMetadata) {
 	defer conn.Close()
+
 	for {
-		var message IncomingMessage
+		var message struct {
+			Type    string          `json:"type"`
+			Payload json.RawMessage `json:"payload"`
+		}
 		if err := conn.ReadJSON(&message); err != nil {
 			if !websocket.IsCloseError(err) && !websocket.IsUnexpectedCloseError(err) {
 				log.Println(err)
 			}
 			return
 		}
-
 		switch message.Type {
-		case MSG_IN_JOIN:
-
-		case MSG_IN_LEAVE:
-
-		case MSG_IN_MOVE:
-
-		default:
 
 		}
 	}
